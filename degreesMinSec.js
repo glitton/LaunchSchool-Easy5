ed4a23932a4aa82c040e98409e6dcfb38bf2ee83
Generosa Litton <glcodeworks@gmail.com>
2024-05-10 23:22:23 -0500

    union and degrees finished for now

--- degreesMinSec.js
+++ degreesMinSec.js
@@ -2,8 +2,17 @@ function dms(degreesFloat) {
   const DEGREE = "\u00B0";
   const MINUTES_PER_DEGREE = 60;
   const SECONDS_PER_MINUTE = 60;
-  //Remove decimal from degrees
-  let degreesInt = Math.floor(degreesFloat);
+  let degreesInt;
+  // if number is negative, convert degrees to positive
+  if (degreesFloat < 0 || degreesFloat > 360) {
+    degreesInt = degreesInRange(degreesFloat);
+    // console.log("degrees", degreesInt);
+  } else {
+    //Remove decimal from degrees
+    degreesInt = Math.floor(degreesFloat);
+    // console.log("degreesFloat", degreesInt);
+  }
+
   let minutes = ((degreesFloat - degreesInt) * MINUTES_PER_DEGREE).toFixed(2);
   let seconds = ((minutes - Math.floor(minutes)) * SECONDS_PER_MINUTE).toFixed(
     2
@@ -18,10 +27,27 @@ function dms(degreesFloat) {
 console.log(dms(360));
 console.log(dms(76.73));
 console.log(dms(93.034773));
+console.log(dms(400));
+console.log(dms(-40));
+console.log(dms(-420));
 
 function padZeroes(number) {
   let numString = String(number);
   return numString.length < 2 ? "0" + numString : numString;
 }
 
+function degreesInRange(number) {
+  if (number > 360) {
+    number = number - 360;
+  }
+  // change this ...
+  if (number < 0) {
+    number = 360 + number;
+    if (number < 0) {
+      number = 360 + number;
+    }
+  }
+  // console.log("degInRange", number);
+  return number;
+}
 // console.log(padZeroes(360));
--- union.js
+++ union.js
@@ -1,11 +1,10 @@
 function union(arr1, arr2) {
   const newArray = arr1.concat(arr2);
-  // console.log(newArray);
   let finalArray = newArray.filter(
-    (item, index) => newArray.indexOf(index) === index
+    (item, index) => newArray.indexOf(item) === index
   );
-
-  console.log(finalArray);
+  return finalArray;
 }
 
-union([1, 3, 5], [3, 6, 9]);
+// console.log(union([1, 3, 5], [3, 6, 9]));
+console.log(union([2, 2, 6], [4, 5, 5, 6]));
